94f8801b89f91c2908dbda20e6f326c60fac3822
chetan.shinde <chetan.shinde@hexpress.net>
2024-08-18 15:14:15 +0530

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build the Next site including SSG
-npm run build
+#npm run build
 
-npm run start
+npm run dev
 # Start the production server
 #pm2-runtime npm --name furni_app -- start 
